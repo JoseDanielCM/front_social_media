18850c8a4f1b056e23b88ff2aa78beb2fbf41b1d
JoseDanielCM <jodacarmon@gmail.com>
2025-02-12 21:59:08 -0500

    Feat take token data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import NavBar from '../Layouts/NavBar'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
-    return (
-        <div className="md:ml-64">
-            hola
-        </div>
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    )
+    useEffect(() => {
+        axios.get("http://localhost:1234/api/user/me", { withCredentials: true })
+            .then(response => {
+                setUser(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error al obtener usuario", error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <p>Cargando usuario...</p>;
+    }
+
+    if (!user) {
+        return <p>No se pudo cargar el usuario.</p>;
+    }
+
+    return (
+        <div className="md:ml-64 md:pl-5">
+            <h1>Bienvenido, {user.username}</h1>
+            <h1>ID: {user.id}</h1>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
